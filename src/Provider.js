eb472a1c99c2e4a6f9903dc3c1414dca6b1bfdab
Bruno Bertolini <dev@brunobertolini.com>
2018-12-20 13:12:13 -0200

    refactor: useMemo and useCallback

--- src/Provider.js
+++ src/Provider.js
@@ -1,18 +1,20 @@
 import * as React from 'react'
-import { useState, useContext, createContext } from 'react'
+import {
+	useState,
+	useContext,
+	useMemo,
+	useCallback,
+	createContext,
+} from 'react'
 import { useAsync } from './useAsync'
 
 const Context = createContext({})
 
 export function Provider({ children, client: fetch }) {
 	const [token, setToken] = useState(false)
-	const client = req => fetch(req, token)
-
-	return (
-		<Context.Provider value={{ client, token, setToken }}>
-			{children}
-		</Context.Provider>
-	)
+	const client = useCallback(req => fetch(req, token), [])
+	const value = useMemo(() => ({ client, token, setToken }), [client, token])
+	return <Context.Provider value={value}>{children}</Context.Provider>
 }
 
 export function useToken() {
