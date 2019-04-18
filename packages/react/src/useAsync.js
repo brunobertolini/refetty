8da8a6f0f6b3e7d2e1b1aaf846a8667037fd3435
Bruno Bertolini <dev@brunobertolini.com>
2019-04-18 09:03:15 -0300

    refactor(react): async returns

--- packages/react/src/useAsync.js
+++ packages/react/src/useAsync.js
@@ -3,7 +3,7 @@ import { asyncState } from '@refetty/fetch'
 import { useStateRx } from './useStateRx'
 
 export const useAsync = promise => {
-	const [, { run, subject }] = useMemo(() => asyncState(promise), [promise])
+	const [subject, run] = useMemo(() => asyncState(promise), [promise])
 	const [state] = useStateRx(subject)
 	return [state, run]
 }
