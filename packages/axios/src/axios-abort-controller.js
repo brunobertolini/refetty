f77bc5cc79e42c3c4de8d4430415856464e05271
Bruno Bertolini <dev@brunobertolini.com>
2019-10-30 11:50:57 -0300

    chore(axios): remove this

--- packages/axios/src/axios-abort-controller.js
+++ packages/axios/src/axios-abort-controller.js
@@ -3,7 +3,9 @@ import axios from 'axios'
 export function AxiosAbortController() {
 	const source = axios.CancelToken.source()
 
-	this.signal = source.token
-	this.abort = source.cancel
-	this.isAborted = axios.isCancel
+	return {
+		signal: source.token,
+		abort: source.cancel,
+		isAborted: axios.isCancel,
+	}
 }
