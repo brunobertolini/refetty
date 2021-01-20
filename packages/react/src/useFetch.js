29baa4c967c7bc27d1c75d2429d8eefdd7f36259
Bruno Bertolini <dev@brunobertolini.com>
2021-01-20 08:55:15 -0300

    feat(react): add complete promise response to useFetch meta data

--- packages/react/src/useFetch.js
+++ packages/react/src/useFetch.js
@@ -2,5 +2,5 @@ import { usePromise } from './usePromise'
 
 export const useFetch = (...args) => {
 	const [result, ...meta] = usePromise(...args)
-	return [result && result.data, ...meta]
+	return [result && result.data, { ...meta, result }]
 }
